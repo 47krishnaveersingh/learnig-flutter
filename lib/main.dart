1027099f6145a3f8fc7dff1b552a6b09635bbb2e
47krishnaveersingh <krishna.vs12345@gmail.com>
2021-09-23 16:22:19 +0530

    comp

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/login",
       routes: {
         "/": (context) => const LoginPage(),
         "/login": (context) => const LoginPage()
